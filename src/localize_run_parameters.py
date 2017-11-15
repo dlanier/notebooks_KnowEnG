9cae4888e0b5fabdb7c65e984584997c576be5d8
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2017-11-15 13:19:22 -0600

    dataframe of all run_parameters

--- src/localize_run_parameters.py
+++ src/localize_run_parameters.py
@@ -4,6 +4,8 @@ functions to convert yaml file data source and output directory references
 to local directory references using a python dict
 """
 import os
+import pandas as pd
+import knpackage.toolbox as kn
 
 def yaml_append(yaml_file_name_full_path, new_key, new_value):
     """ Usage:  yaml_append(yaml_file_name_full_path, new_key, new_value)  """
@@ -76,3 +78,61 @@ def set_local_run_parameters(run_parameters, local_dict):
                     run_parameters[k] = os.path.join(key_value, f_name)
                 
     return run_parameters
+
+
+def get_parameter_keys_dict(yaml_dir, parameter_key_dict={}, methods_list=[]):
+    """  get the list of yaml files in the yaml_dir, the list of methods in all those, and a dictionary of key types
+
+    Args:
+        yaml_dir:           path the the yaml files
+        parameter_key_dict: (optional) dictionary to add key types
+        methods_list:       (optional) list of methods to append
+
+    Returns:
+        yaml_files_list:    the list of yaml files in the yaml_dir
+        methods_list:       the list of methods in the yaml files
+        parameter_key_dict: key-name: key-type   dictionary
+    """
+    type_dict = {int: 'int', float: 'real', str: 'string', list: 'list'}
+
+    yaml_files_list = []
+    for f in os.listdir(yaml_dir):
+        ff = os.path.join(yaml_dir, f)
+        some_nombre, nombre_ext = os.path.splitext(f)
+        if os.path.isfile(ff) and nombre_ext == '.yml':
+            yaml_files_list.append(f)
+
+    for f_name in yaml_files_list:
+        run_parameters = kn.get_run_parameters(yaml_dir, f_name)
+        for k, v in run_parameters.items():
+            if k not in parameter_key_dict.keys():
+                parameter_key_dict[k] = type_dict[type(v)]
+            if k == 'method' and k not in methods_list:
+                methods_list.append(v)
+
+    methods_list = list(set(methods_list))
+    return parameter_key_dict, methods_list, yaml_files_list
+
+
+def get_yaml_df(yaml_dir, UNUSED_FILL='not used'):
+    """ read the yaml files in 'yaml_dir' and constuct a datafrme with rows of keys, columns of file names and
+        filled with the each yaml file values.
+    Args:
+        yaml_dir:       path the the yaml files
+        UNUSED_FILL:    string to use for fill where a yaml file has no entry for a key (row name)
+    """
+    key_dict, methods_list, yaml_files_list = get_parameter_keys_dict(yaml_dir)
+    if 'run_file' in key_dict.keys():
+        key_dict.pop('run_file')
+    yaml_file_df = pd.DataFrame(data=None, index=key_dict.keys(), columns=None)
+
+    for f_name in yaml_files_list:
+        run_parameters = kn.get_run_parameters(yaml_dir, f_name)
+        if 'run_file' in run_parameters.keys():
+            run_parameters.pop('run_file')
+
+        yaml_file_df[f_name] = UNUSED_FILL
+        for k, v in run_parameters.items():
+            yaml_file_df[f_name].loc[k] = v
+
+    return yaml_file_df
