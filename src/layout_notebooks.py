8f3e2ac632b7311df8bd18b30b9d62600b6697d8
dlanier <dlanier@users.noreply.github.com>
2017-12-11 10:53:30 -0600

    add license statement

--- src/layout_notebooks.py
+++ src/layout_notebooks.py
@@ -1,3 +1,13 @@
+"""
+Copyright 2017 University of Illinois Board of Trustees. All Rights Reserved.
+Licensed under the MIT  license (the "License");
+You may not use this file except in compliance with the License.
+The License is included in the distribution as License.txt file.
+ 
+Software  distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and  limitations under the License.
+"""
 import os
 import pandas as pd
 from pandas.io.common import EmptyDataError
