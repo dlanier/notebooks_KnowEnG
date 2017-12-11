fa275f99c6abdac4ed79eb855d8ddf9a56891f4c
dlanier <dlanier@users.noreply.github.com>
2017-12-11 10:54:10 -0600

    add license statement

--- src/Signature_Analysis_notebook_tools.py
+++ src/Signature_Analysis_notebook_tools.py
@@ -1,4 +1,13 @@
-""" Signature_Analysis_notebook_tools.py """
+"""
+Copyright 2017 University of Illinois Board of Trustees. All Rights Reserved.
+Licensed under the MIT  license (the "License");
+You may not use this file except in compliance with the License.
+The License is included in the distribution as License.txt file.
+ 
+Software  distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and  limitations under the License.
+"""
 import os
 import sys
 # import pandas as pd
