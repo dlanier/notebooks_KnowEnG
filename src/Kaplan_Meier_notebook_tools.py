d34ff07efedb99c33d97972a20bb849e0dbb94bf
dlanier <dlanier@users.noreply.github.com>
2017-12-11 10:54:46 -0600

    add license statement

--- src/Kaplan_Meier_notebook_tools.py
+++ src/Kaplan_Meier_notebook_tools.py
@@ -1,8 +1,12 @@
-""" 
-KnowEnG's Kaplan_Meier notebook code
-
-Knowledge Engine for Genomics (KnowEnG), an NIH BD2K Center of Excellence
-lanier4@illinois.edu 
+"""
+Copyright 2017 University of Illinois Board of Trustees. All Rights Reserved.
+Licensed under the MIT  license (the "License");
+You may not use this file except in compliance with the License.
+The License is included in the distribution as License.txt file.
+ 
+Software  distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and  limitations under the License.
 """
 import time
 import numpy as np
